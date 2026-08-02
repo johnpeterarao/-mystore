93d0fc191bbf5ad6e460f8d315058e7bc40e417b
john <johnpeterarao@gmail.com>
2026-08-02 11:42:03 +0800

    setup adjustment

--- react/vite.config.js
+++ react/vite.config.js
@@ -1,18 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
   build: {
     outDir: "../assets",
     emptyOutDir: false,
     rollupOptions: {
       input: "./src/main.jsx",
       output: {
-        entryFileNames: "lookbook.js",
-        assetFileNames: "lookbook.css"
+        entryFileNames: "reactjs.js",
+        assetFileNames: "reactcss.css"
       }
     }
   }
-})
+});
