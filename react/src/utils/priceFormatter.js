489fc73f777c324fb79996fa1b3d8b2b1786bc26
john <johnpeterarao@gmail.com>
2026-08-02 14:36:20 +0800

    price formatter and lookbook in homepage

---
+++ react/src/utils/priceFormatter.js
@@ -0,0 +1,21 @@
+export function formatPrice(amount, currency, country) {
+
+  const localeMap = {
+    AU: "en-AU",
+    JP: "ja-JP"
+  };
+
+  const price = new Intl.NumberFormat(
+    localeMap[country] || "en",
+    {
+      style: "currency",
+      currency,
+    }
+  ).format(Number(amount));
+
+  const priceSuffix = country == 'AU' ? 'AUD' : 'JPY';
+
+  return `${price} ${priceSuffix}`
+
+
+}
