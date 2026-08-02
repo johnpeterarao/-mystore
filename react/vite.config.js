63115f86ec1959205680f18a3c7f24f02a327914
john <johnpeterarao@gmail.com>
2026-08-02 11:05:54 +0800

    simple connection test

--- react/vite.config.js
+++ react/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "../assets",
+    emptyOutDir: false,
+    rollupOptions: {
+      input: "./src/main.jsx",
+      output: {
+        entryFileNames: "lookbook.js",
+        assetFileNames: "lookbook.css"
+      }
+    }
+  }
 })
